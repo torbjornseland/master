a2fe846850f608338d3497f8c40a390256b2adce
Torbjørn Seland <torbjornseland@hotmail.com>
2014-07-04 13:07:01 +0200

    worked on stability

--- prosjekt_inf5631/amplification_factor.py
+++ prosjekt_inf5631/amplification_factor.py
@@ -1,26 +1,29 @@
+import numpy as np
 import matplotlib.pyplot as plt
 
 def A_fe(dt):
-    return 1+dt*a
+    return 1-dt*a
 
 def A_be(dt):
-    return 1./(1-dt*a)
+    return 1./(1+dt*a)
+
+def exact(dt):
+    return np.exp(-a*dt)
 
 a = 1
-dt_list = [0.9,0.8,0.65,0.5,0.25,0.1,0.05,0.01,0.005,0.001]
+dt_list = np.linspace(0,3,20)
 
-fe = []
-be = []
-for i in dt_list:
-    fe.append(A_fe(i))
-    be.append(A_be(i))
+fe = A_fe(dt_list) 
+be = A_be(dt_list) 
+u_e = exact(dt_list) 
 
 plt.plot(dt_list,fe,label="Forward Euler")
 plt.plot(dt_list,be,label="Backward Euler")
+plt.plot(dt_list,u_e,label="Exact solution")
 plt.xlabel("a*dt")
 plt.ylabel("Amplification factor")
-ax = plt.gca()
-ax.invert_xaxis()
+#ax = plt.gca()
+#ax.invert_xaxis()
 plt.legend()
 plt.savefig("images/amplification_factor.png")
 plt.show()
