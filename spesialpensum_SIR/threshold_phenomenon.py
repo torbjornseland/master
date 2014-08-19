efa5de71f0ad72c6de3602f6d0837a6bd03760b2
Torbjørn Seland <torbjornseland@hotmail.com>
2014-08-19 15:42:52 +0200

    worked on biological models

--- spesialpensum_SIR/threshold_phenomenon.py
+++ spesialpensum_SIR/threshold_phenomenon.py
@@ -10,14 +10,14 @@ rho = a/r
 
 plt.plot([N,0],[0,N],label="Possible startposition")
 plt.plot([rho,rho],[0,N-rho],label="Threshold value")
-S0_list = [100,90,80,70,60,50,40,30,20]
+S0_list = [100,90,80]#,70,60,50,40,30,20]
 for j in S0_list:
     S_0 = j
     I_0 = N-S_0
     R_0 = 0
 
     T = 10
-    Nt = 100
+    Nt = 1000
 
     t = np.linspace(0,T,Nt+1)
     dt = t[1]-t[0]
@@ -29,11 +29,15 @@ for j in S0_list:
     I[0] = I_0
     R[0] = R_0
 
-
+    print "round"
     for i in range(Nt):
         S[i+1] = S[i] - dt*r*S[i]*I[i] 
         I[i+1] = I[i] + dt*(r*S[i]*I[i]-a*I[i])
         R[i+1] = R[i] + dt*a*I[i]
+        if(I[i]>I[i+1] and I[i]>I[i-1]):
+            print "max",I[i]
+        #print "constant",S[i+1]+I[i+1]-rho*np.log(S[i+1])
+
 
 
     plt.plot(S,I)
@@ -41,7 +45,7 @@ plt.axis([0,N,0,N])
 plt.xlabel("S")
 plt.ylabel("I")
 plt.legend()
-plt.savefig("plots/threshold_phenomenon.png")
+#plt.savefig("plots/threshold_phenomenon.png")
 plt.show()
 
 
