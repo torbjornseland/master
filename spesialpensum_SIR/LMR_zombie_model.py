684803accc0b0f415dfd62a46bb99eb56de43d0d
Torbjørn Seland <torbjornseland@hotmail.com>
2014-09-03 16:23:30 +0200

    hysterical phase

--- spesialpensum_SIR/LMR_zombie_model.py
+++ spesialpensum_SIR/LMR_zombie_model.py
@@ -2,12 +2,12 @@ from math import exp
 import numpy as np
 import matplotlib.pyplot as plt
 
-def run_program(Sigma,beta,delta_S,delta_I,rho,zeta,alpha,a,sigma,attacks,filename,plot,print_):
+def run_program(Sigma,beta,delta_S,delta_I,rho,zeta,alpha,a,sigma,attacks,filename,plot,print_,S_0,I_0,Z_0,R_0):
     def omega(t, a, sigma, T):
         return a*sum(exp(-0.5*(t-T[i])**2/sigma) for i in range(len(T)))
 
     dt = 0.001
-    D = 3
+    D = 30
     n = int(D/dt)
     T_tot = np.linspace(0,D,n+1)
     # time step measured in hours
@@ -18,10 +18,10 @@ def run_program(Sigma,beta,delta_S,delta_I,rho,zeta,alpha,a,sigma,attacks,filena
     Z = np.zeros(n+1)
     R = np.zeros(n+1)
     # initial conditions:
-    S[0] = 621
-    I[0] = 0
-    Z[0] = 1
-    R[0] = 0
+    S[0] = S_0
+    I[0] = I_0
+    Z[0] = Z_0
+    R[0] = R_0
     # step equations forward in time:
     for i in range(n):
         t = i*dt
@@ -36,11 +36,11 @@ def run_program(Sigma,beta,delta_S,delta_I,rho,zeta,alpha,a,sigma,attacks,filena
         plt.plot(T_tot,I,label="I")
         plt.plot(T_tot,Z,label="Z")
         plt.plot(T_tot,R,label="R")
-        plt.axis([0,3,0,800])
+        plt.axis([0,D,0,800])
         plt.xlabel("days")
         plt.ylabel("humans")
         plt.legend()
-        #plt.savefig(filename)
+        plt.savefig(filename)
         plt.show()
     if print_:
         print "S_n",S[n]
@@ -61,10 +61,15 @@ if __name__ == "__main__":
     Sigma = 3.45*10**(-5) ;beta = 0.3; delta_S = 2.5*10**(-5);delta_I = delta_S ;rho = 1;zeta = 0
     alpha = 0.;a = 0;sigma = 0.5;attacks = [5, 10, 18];filename = "plots/WD_zombie_initial_1.png"
     run_program(Sigma,beta,delta_S,delta_I,rho,zeta,alpha,a,sigma,attacks,filename)
-    """
+    
     Sigma = 3.45*10**(-5) ;beta = 0.01155; delta_S = 2.5*10**(-5);delta_I = delta_S ;rho = 1.37;zeta = 0
     alpha = 0.00044;a = 0;sigma = 0.5;attacks = [5, 10, 18];filename = "plots/WD_zombie_initial_2.png"; plot = True;print_= True
     run_program(Sigma,beta,delta_S,delta_I,rho,zeta,alpha,a,sigma,attacks,filename,plot,print_)
+    
+    Sigma = 3.45*10**(-5) ;beta = 0.000011; delta_S = 2.5*10**(-5);delta_I = delta_S ;rho = 1.5;zeta = 0
+    alpha = 0.000208;a = 0;sigma = 0.5;attacks = [5, 10, 18];filename = "plots/WD_zombie_hysterical_1.png"; 
+    plot = True;print_= True; S_0 = 71.3; I_0 = 230.0; Z_0 = 298.9; R_0 = 21
+    run_program(Sigma,beta,delta_S,delta_I,rho,zeta,alpha,a,sigma,attacks,filename,plot,print_,S_0,I_0,Z_0,R_0)
     """
     def check_param(beta_list,rho_list,alpha_list,nr, title):
         S_n_list = []
@@ -75,46 +80,35 @@ if __name__ == "__main__":
                 for j in alpha_list:
                     Sigma = 3.45*10**(-5) ;beta = i; delta_S = 2.5*10**(-5);delta_I = delta_S ;rho = k;zeta = 0
                     alpha = j;a = 0;sigma = 0.5;attacks = [5, 10, 18];filename = "plots/WD_zombie_initial_2.png"
-                    S_n,R_n = run_program(Sigma,beta,delta_S,delta_I,rho,zeta,alpha,a,sigma,attacks,filename,plot=False,print_=False)
-                    if(S_n < 73 and S_n > 69) and (R_n < 22 and R_n> 18):
-                        print i,k,j
+                    S_0 = 71.3; I_0 = 230.0; Z_0 = 298.9; R_0 = 21;plot=False;print_ = False
+                    S_n,R_n = run_program(Sigma,beta,delta_S,delta_I,rho,zeta,alpha,a,sigma,attacks,filename,plot,print_,S_0,I_0,Z_0,R_0)
+                    if(S_n < 63 and S_n > 61) and (R_n < 201 and R_n> 199):
+                        print "beta = %f,rho=%f, alpha=%f, S_n = %f, R_n 0 %f" % (i,k,j,S_n,R_n)
                     S_n_list.append(S_n)
                     R_n_list.append(R_n)
         plt.subplot(2,2,nr)
         plt.plot(S_n_list, R_n_list, 'ro',label= "data from variable")
-        plt.plot([71], [20], 'bo',label= "exact data")
-        plt.axis([0,300,0,50])
+        plt.plot([62], [200], 'bo',label= "exact data")
+        plt.axis([60,63,190,204])
         plt.xlabel("susceptible")
         plt.ylabel("removed")
         plt.legend()
         plt.title(title)
     
     
-    beta_list = np.linspace(0.010,0.013,5)
-    rho_list = [1.37]#np.linspace(0.5,2,20)
-    alpha_list = [0.00044]#np.linspace(0.01,0.15,20)
-    nr = 1
-    check_param(beta_list,rho_list,alpha_list,nr,"beta = [0.01,0.013]")
+    beta_list = np.linspace(0.00001,0.000012,5)
+    rho_list = np.linspace(0.5,2,4)
+    alpha_list = np.linspace(0.0002,0.00021,5)
     
-    beta_list = [0.01155]#np.linspace(0.,0.5,100)
-    rho_list = np.linspace(1.2,1.6,5)
-    alpha_list = [0.00044]#np.linspace(0.01,0.15,20)
-    nr = 2
-    check_param(beta_list,rho_list,alpha_list,nr,"varrho = [1.2,1.6]")
+    beta_list_1 = [0.000011]#np.linspace(0.,0.5,100)
+    rho_list_1 = [1.5]#np.linspace(0.5,2,20)
+    alpha_list_1 = [0.000208]#np.linspace(0.01,0.15,20)
     
-    beta_list = [0.01155]#np.linspace(0.,0.5,100)
-    rho_list = [1.37]#np.linspace(0.5,2,20)
-    alpha_list = np.linspace(0.0003,0.0007,5)
-    nr = 3
-    check_param(beta_list,rho_list,alpha_list,nr,"alpha = [0.0003, 0.0007]")
+    check_param(beta_list,rho_list_1,alpha_list_1,1,"beta = [0.01,0.013]")
+    check_param(beta_list_1,rho_list,alpha_list_1,2,"varrho = [1.2,1.6]")
+    check_param(beta_list_1,rho_list_1,alpha_list,3,"alpha = [0.0003, 0.0007]")
+    check_param(beta_list,rho_list,alpha_list,4,"All parameters")
     
-    beta_list = np.linspace(0.010,0.013,5)
-    rho_list = np.linspace(1.2,1.6,5)
-    alpha_list = np.linspace(0.0003,0.0007,5)
-    nr = 4
-    check_param(beta_list,rho_list,alpha_list,nr,"All parameters")
-    
-    plt.savefig("check_parameters.png")
+    plt.savefig("plots/check_parameters_hysterical_2.png")
     plt.show()
-    """
-    
+        
