3fe37e81ce2b75aa0c9a285011404dc7f6cc5b8e
Torbjørn Seland <torbjornseland@hotmail.com>
2014-09-17 16:15:43 +0200

    worked with counter attack and three phases

--- spesialpensum_SIR/LMR_zombie_model.py
+++ spesialpensum_SIR/LMR_zombie_model.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 def omega(t, a, sigma, T):
     return a*sum(exp(-0.5*(t-T[i])**2/sigma) for i in range(len(T)))
 
-def run_program(Sigma,beta,delta_S,delta_I,rho,zeta,alpha,a,sigma,attacks,filename,plot,print_,S_0,I_0,Z_0,R_0,D):
+def run_program(Sigma,beta,delta_S,delta_I,rho,zeta,alpha,a,sigma,attacks,filename,plot,print_,S_0,I_0,Z_0,R_0,D,mu, phases, ph_print):
 
     dt = 0.001
     #D = 30
@@ -24,30 +24,49 @@ def run_program(Sigma,beta,delta_S,delta_I,rho,zeta,alpha,a,sigma,attacks,filena
     Z[0] = Z_0
     R[0] = R_0
     # step equations forward in time:
-    for i in range(n):
-        t = i*dt
-        omega_t = omega(t, a, sigma, attacks)
-        S[i+1] = S[i] + dt*(Sigma - beta*S[i]*Z[i] - delta_S*S[i])
-        I[i+1] = I[i] + dt*(beta*S[i]*Z[i] - rho*I[i] - delta_I*I[i])
-        Z[i+1] = Z[i] + dt*(rho*I[i] - (alpha + omega_t)*S[i]*Z[i] + zeta*R[i])
-        R[i+1] = R[i] + dt*(delta_S*S[i] - zeta*R[i] + delta_I*I[i] + (alpha + omega_t)*S[i]*Z[i])
+    if phases:
+        for i in range(n):
+            t = i*dt
+            for j in range(len(phases)):
+                if t < phases[j]:
+                    val = j
+                    break
+            print val
+            omega_t = omega(t, a, sigma, attacks)
+            S[i+1] = S[i] + dt*(Sigma - (beta[val]+mu*omega_t)*S[i]*Z[i] - delta_S*S[i])
+            I[i+1] = I[i] + dt*((beta[val]+ mu*omega_t)*S[i]*Z[i] - rho[val]*I[i] - delta_I*I[i])
+            Z[i+1] = Z[i] + dt*(rho[val]*I[i] - (alpha[val] + omega_t)*S[i]*Z[i] + zeta*R[i])
+            R[i+1] = R[i] + dt*(delta_S*S[i] - zeta*R[i] + delta_I*I[i] + (alpha[val] + omega_t)*S[i]*Z[i])
+    else:
+        for i in range(n):
+            t = i*dt
+            omega_t = omega(t, a, sigma, attacks)
+            S[i+1] = S[i] + dt*(Sigma - (beta+mu*omega_t)*S[i]*Z[i] - delta_S*S[i])
+            I[i+1] = I[i] + dt*((beta+ mu*omega_t)*S[i]*Z[i] - rho*I[i] - delta_I*I[i])
+            Z[i+1] = Z[i] + dt*(rho*I[i] - (alpha + omega_t)*S[i]*Z[i] + zeta*R[i])
+            R[i+1] = R[i] + dt*(delta_S*S[i] - zeta*R[i] + delta_I*I[i] + (alpha + omega_t)*S[i]*Z[i])
 
     if plot:
         plt.plot(T_tot,S,label="S")
         plt.plot(T_tot,I,label="I")
         plt.plot(T_tot,Z,label="Z")
         plt.plot(T_tot,R,label="R")
-        plt.axis([0,D,0,400])
+        plt.axis([0,D,0,800])
         plt.xlabel("days")
         plt.ylabel("humans")
         plt.legend()
         plt.savefig(filename)
         plt.show()
     if print_:
-        print "S_n",S[n]
-        print "I_n",I[n]
-        print "Z_n",Z[n]
-        print "R_n",R[n]
+        ph_list = []
+        for p in ph_print:
+            ph_list.append(int(p/dt))
+        for i in ph_list:
+            print "---------------------"
+            print "S_n",S[i]
+            print "I_n",I[i]
+            print "Z_n",Z[i]
+            print "R_n",R[i]
     return S[n],R[n]
 
 if __name__ == "__main__":
@@ -71,11 +90,30 @@ if __name__ == "__main__":
     alpha = 0.000208;a = 0;sigma = 0.5;attacks = [5, 10, 18];filename = "plots/WD_zombie_hysterical_1.png"; 
     plot = True;print_= True; S_0 = 71.3; I_0 = 230.0; Z_0 = 298.9; R_0 = 21
     run_program(Sigma,beta,delta_S,delta_I,rho,zeta,alpha,a,sigma,attacks,filename,plot,print_,S_0,I_0,Z_0,R_0)
-    """
+    
     Sigma = 3.45*10**(-5) ;beta = 0.000011; delta_S = 2.5*10**(-5);delta_I = delta_S ;rho = 1.5;zeta = 0
     alpha = 0.000208;a = 0.00103;sigma = 0.005;attacks = [0.625];filename = "plots/WD_zombie_counter_1.png"; 
-    plot = True;print_= True; S_0 = 62; I_0 = 1; Z_0 = 359; R_0 = 200; D = 1
+    plot = True;print_= True; S_0 = 62; I_0 = 1; Z_0 = 359; R_0 = 200; D = 1, mu = 4./30
     run_program(Sigma,beta,delta_S,delta_I,rho,zeta,alpha,a,sigma,attacks,filename,plot,print_,S_0,I_0,Z_0,R_0,D)
+    
+    Sigma = 3.45*10**(-5) ;beta = 0.000011; delta_S = 2.5*10**(-5);delta_I = delta_S ;rho = 1.5;zeta = 0
+    alpha = 0.000208;a = 0.0073;sigma = 0.005;attacks = [0.625];filename = "plots/WD_zombie_counter_2.png"; 
+    plot = True;print_= True; S_0 = 62; I_0 = 1; Z_0 = 359; R_0 = 200; D = 1; mu = 0.14
+    run_program(Sigma,beta,delta_S,delta_I,rho,zeta,alpha,a,sigma,attacks,filename,plot,print_,S_0,I_0,Z_0,R_0,D,mu)
+    
+    Sigma = 3.45*10**(-5) ;beta = 0.000011; delta_S = 2.5*10**(-5);delta_I = delta_S ;rho = 1.5;zeta = 0
+    alpha = 0.000208;a = 0.0073;sigma = 0.005;filename = "plots/WD_zombie_counter_series.png"; 
+    plot = True;print_= True; S_0 = 62; I_0 = 1; Z_0 = 359; R_0 = 200; D = 200; mu = 0.14
+    attacks = []
+    for i in range(100):
+        attacks.append(i*2)
+    run_program(Sigma,beta,delta_S,delta_I,rho,zeta,alpha,a,sigma,attacks,filename,plot,print_,S_0,I_0,Z_0,R_0,D,mu)
+    """
+    Sigma = 3.45*10**(-5) ;beta = [0.01155, 0.000011]; delta_S = 2.5*10**(-5);delta_I = delta_S ;rho = [1.37,1.5] ;zeta = 0
+    alpha = [0.00044,0.000208];a = 0.0073; sigma = 0.005;filename = "plots/WD_zombie_all_phases_1.png"; 
+    plot = True;print_= True; S_0 = 621; I_0 = 0; Z_0 = 1; R_0 = 0; D = 34; mu = 0.14; attacks = [33.125]; phases = [3,34]; 
+    ph_print = [0,3,33,34]
+    run_program(Sigma,beta,delta_S,delta_I,rho,zeta,alpha,a,sigma,attacks,filename,plot,print_,S_0,I_0,Z_0,R_0,D,mu,phases, ph_print)
     """
     def check_param(beta_list,rho_list,alpha_list,nr, title):
         S_n_list = []
