26dd730d20ce63742425badaecb70122902d716f
Torbjørn Seland <torbjornseland@hotmail.com>
2014-11-25 16:46:59 +0100

    add works

--- 3_Random_walk/random_diffusion_eq.py
+++ 3_Random_walk/random_diffusion_eq.py
@@ -2,6 +2,7 @@ from xorshift import *
 import random as rd
 import numpy as np
 import matplotlib.pyplot as plt
+import sympy as sm
 
 X = 123456789
 Y = 362436069
@@ -18,13 +19,12 @@ def gauss_sig(x,sig):
     return (1/(sig*np.sqrt(2*np.pi)))*np.exp(-(x**2)/(2*sig**2))
 
 class walker:
-    def __init__(self,X,Y,Z,W,mu,sig):
+    def __init__(self,X,Y,Z,W,mu,sig,step):
         self.pos = 0 #rd.gauss(mu,sig)
         self.random_gen = xor_shift_32(X,Y,Z,W)
-        self.step = 0.1
+        self.step = step
 
     def walk(self):
-
         if self.random_gen.random_value() > 0.5:
             self.pos += self.step
         else:
@@ -33,48 +33,59 @@ class walker:
         return self.pos
 
 
-#Add a list of walkers:
-N = 1000 # Number of walkers
-T = 2 #
-X = 10
-dt = 0.1
-dx = l = 0.1
-Tn = int(T/dt)
-print "Tn"
-walkers = []
-max_int = np.iinfo(np.uint32).max
-D = 1
-sig = np.sqrt(2*D*T)
-for i in range(N):
-    X_rand = rd.randint(0,max_int)
-    Y_rand = rd.randint(0,max_int)
-    Z_rand = rd.randint(0,max_int)
-    W_rand = rd.randint(0,max_int)
-    walkers.append(walker(X_rand,Y_rand,Z_rand,W_rand,0,sig))
 
-#Walk Tn steps
-for j in range(Tn):
-    print "j",j
-    for walker in walkers:
-        walker.walk()
-x = np.linspace(-X,X,201)
-y = np.zeros(201)
-width = 0.1
-sig_num = 0
-for walker in walkers:
-    #print "walk",walker.get_pos()
-    #print(walker.get_pos()+10)/dt
-    y[(walker.get_pos()+10)/dt] += 1
-    if(walker.get_pos()> -sig) and (walker.get_pos()< sig):
-        sig_num += 1
+def run_random(title,savefile,T):
+    #Add a list of walkers:
+    N = 10000 # Number of walkers
+    #T = 2 
+    X = 10
+    dt = 0.01
+    dx = l = 0.01
+    Tn = int(T/dt)
+    print "Tn"
+    walkers = []
+    max_int = np.iinfo(np.uint32).max
+    D = (dx**2/dt)*0.5
+    sig = np.sqrt(((dx**2)*T)/dt)
+    for i in range(N):
+        X_rand = rd.randint(0,max_int)
+        Y_rand = rd.randint(0,max_int)
+        Z_rand = rd.randint(0,max_int)
+        W_rand = rd.randint(0,max_int)
+        walkers.append(walker(X_rand,Y_rand,Z_rand,W_rand,0,sig,dx))
 
-max_h = np.max(y)/float(N)
-plt.bar(x,y/float(N),width)
-#plt.axis([-10,10,0,max_h*1.1])
-def gauss(x,T):
-    return (1/np.sqrt(4*np.pi*D*T))*np.exp(-(x**2)/(4*D*T))
+    #Walk Tn steps
+    for j in range(Tn):
+        #print "j",j
+        for walkeri in walkers:
+            walkeri.walk()
+    x = np.linspace(-X,X,2001)
+    y = np.zeros(2001)
+    width = 0.02
+    sig_num = 0
+    av_dis = 0
+    for walkeri in walkers:
+        y[(walkeri.get_pos()+10E-15+10)/dx] += 1
+        if(walkeri.get_pos()> -sig) and (walkeri.get_pos()< sig):
+            sig_num += 1
+        av_dis += walkeri.get_pos()
 
-plt.plot(x,gauss(x,T))
-print "sig_num",sig_num
+    max_h = np.max(y)/float(N)
+    plt.bar(x,y/float(N),width,align='center',label="random walk")
+    def gauss(x,T):
+        return 0.02*(1/np.sqrt(4*np.pi*D*T))*np.exp(-(x**2)/(4*D*T))
 
-plt.show()
+    plt.plot(x,gauss(x,T),'r',label="gauss function")
+    #plt.plot(x,gauss(x,T*0.5),'g')
+    plt.axis([-1,1,0,0.07])
+    print "sig_num",sig_num
+    print "average displacement", av_dis/float(N)
+
+    plt.title(title)
+    plt.legend()
+    plt.savefig(savefile)
+    plt.show()
+
+#T_list = [2,8]
+#for i in T_list:
+#    run_random("time = %s" % i ,"plots/random_%s_t.png" % i ,i)
