f87f8c7cc5990feb60c65f772e8b350c12517aff
Home <mac@example.com>
2014-12-18 15:57:32 +0100

    worked on the finish of the thesis

--- 2_PDE_Fisher_Kolmogoroff/2_src/verify_sol.py
+++ 2_PDE_Fisher_Kolmogoroff/2_src/verify_sol.py
@@ -46,6 +46,85 @@ def test_constant_solution():
     assert difference < tol
 
 def test_manufactured_solution(T,Nt,X,Nx):
+    
+    def exact_solution_S(t,x):
+        return np.cos(np.pi*x)*t
+
+    def exact_solution_I(t,x):
+        return np.cos(np.pi*x)*t
+
+    def exact_solution_R(t,x):
+        return np.cos(np.pi*x)*t
+
+
+    def beta(t,x):
+        return exact_solution_S(t,x)*exact_solution_I(t,x)/exact_solution_R(t,x)
+   
+    def f(t,x):
+        return (t**2*np.cos(np.pi*x) + (np.pi/float(X))**2*t + 1)*np.cos(np.pi*x) 
+
+    def g(t,x):
+        return (lam*t - t**2*np.cos(np.pi*x) + (np.pi/float(X))**2*t + 1)*np.cos(np.pi*x)
+
+    def h(t,x):
+        return (-lam*t + (np.pi/float(X))**2*t + 1)*np.cos(np.pi*x)
+        
+
+    dx = X/float(Nx)
+    dt = T/float(Nt)
+    S_1 = exact_solution_S(0,np.linspace(0-dx,X+dx,Nx+3))
+    I_1 = exact_solution_I(0,np.linspace(0-dx,X+dx,Nx+3))
+    R_1 = exact_solution_R(0,np.linspace(0-dx,X+dx,Nx+3))
+     
+    t,x,S,I,R = simple_PDE(T,Nx,Nt,X,lam,beta,S_1,I_1,R_1,f,g,h)
+    S_e = exact_solution_S(t[-1],x)
+    I_e = exact_solution_I(t[-1],x)
+    R_e = exact_solution_R(t[-1],x)
+    
+    difference_S = abs(S_e - S).max()  # max deviation
+    difference_I = abs(I_e - I).max()  # max deviation
+    difference_R = abs(R_e - R).max()  # max deviation
+
+    return difference_S,difference_I,difference_R
+     
+def solver(I, a, b, T, dt, theta):
+    """
+    Solve u'=-a(t)*u + b(t), u(0)=I,
+    for t in (0,T] with steps of dt.
+    a and b are Python functions of t.
+    """
+    dt = float(dt)            # avoid integer division
+    Nt = int(round(T/dt))     # no of time intervals
+    T = Nt*dt                 # adjust T to fit time step dt
+    u = zeros(Nt+1)           # array of u[n] values
+    t = linspace(0, T, Nt+1)  # time mesh
+
+    u[0] = I                  # assign initial condition
+    for n in range(0, Nt):    # n=0,1,...,Nt-1
+        u[n+1] = ((1 - dt*(1-theta)*a(t[n]))*u[n] +\
+                  dt*(theta*b(t[n+1]) + (1-theta)*b(t[n])))/\
+                  (1 + dt*theta*a(t[n+1]))
+    return u, t
+
+#test_constant_solution()
+a = 15
+T = 1; Nt = [200,200*(2**2),200*(4**2),200*(8**2)]
+X = 1; Nx = [10,10*2,10*4,10*8]
+#Nt = [400,400*(2**2),400*(3**2),400*(4**2)]
+#Nt = [800,800*(2**2),800*(3**2),800*(4**2)]
+#Nt = [1600,1600*(2**2),1600*(3**2),1600*(4**2)]
+for i in range(4):
+    d_s,d_i,d_r = test_manufactured_solution(T,Nt[i],X,Nx[0])
+    if i > 0:
+        #print "-----------------"
+        #print "dt", T/float(Nt[i])     
+        #print "dt_1", T/float(Nt[i-1])     
+        #print "d_s",d_s
+        #print "d_s_1",d_s_1
+        print "r",np.log(d_s_1/d_s)/np.log((T/float(Nt[i-1]))/(T/float(Nt[i])))
+    d_s_1 = d_s
+
+def test_manufactured_solution2(T,Nt,X,Nx):
     """
     Test problem where u=c*t+I is the exact solution, to be
     reproduced (to machine precision) by any relevant method.
@@ -110,40 +189,4 @@ def test_manufactured_solution(T,Nt,X,Nx):
     #tol = 1E-14
     #assert difference < tol
     return difference_S,difference_I,difference_R
-     
-def solver(I, a, b, T, dt, theta):
-    """
-    Solve u'=-a(t)*u + b(t), u(0)=I,
-    for t in (0,T] with steps of dt.
-    a and b are Python functions of t.
-    """
-    dt = float(dt)            # avoid integer division
-    Nt = int(round(T/dt))     # no of time intervals
-    T = Nt*dt                 # adjust T to fit time step dt
-    u = zeros(Nt+1)           # array of u[n] values
-    t = linspace(0, T, Nt+1)  # time mesh
-
-    u[0] = I                  # assign initial condition
-    for n in range(0, Nt):    # n=0,1,...,Nt-1
-        u[n+1] = ((1 - dt*(1-theta)*a(t[n]))*u[n] +\
-                  dt*(theta*b(t[n+1]) + (1-theta)*b(t[n])))/\
-                  (1 + dt*theta*a(t[n+1]))
-    return u, t
-
-#test_constant_solution()
-a = 15
-T = 1; Nt = [200,200*(2**2),200*(4**2),200*(8**2)]
-X = 1; Nx = [10,10*2,10*4,10*8]
-#Nt = [400,400*(2**2),400*(3**2),400*(4**2)]
-#Nt = [800,800*(2**2),800*(3**2),800*(4**2)]
-#Nt = [1600,1600*(2**2),1600*(3**2),1600*(4**2)]
-for i in range(4):
-    d_s,d_i,d_r = test_manufactured_solution(T,Nt[i],X,Nx[0])
-    if i > 0:
-        #print "-----------------"
-        #print "dt", T/float(Nt[i])     
-        #print "dt_1", T/float(Nt[i-1])     
-        #print "d_s",d_s
-        #print "d_s_1",d_s_1
-        print "r",np.log(d_s_1/d_s)/np.log((T/float(Nt[i-1]))/(T/float(Nt[i])))
-    d_s_1 = d_s
+ 
